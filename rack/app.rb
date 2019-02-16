390a9b075c2ee507b8a4a910ea2313ab4f36ef9b
KhIgorLe <43117537+KhIgorLe@users.noreply.github.com>
2019-02-16 13:13:14 +0200

    time (#1)

---
+++ rack/app.rb
@@ -0,0 +1,37 @@
+class App
+  def call(env)
+    request  = Rack::Request.new(env)
+
+    if request.path_info == "/time"
+      time_response(request)
+    else
+      response(404, "The requested resource is not available")
+    end
+  end
+
+  private
+
+  def response(status, body)
+    [status,  { 'Content-Type' => 'text/plain' }, [body]]
+  end
+
+  def time_response(request)
+    format_string = request.params['format']
+
+    if format_string
+      check_format_response(format_string)
+    else
+      response(400, "Format not found")
+    end
+  end
+
+  def check_format_response(format_string)
+    time_format = TimeFormat.new(format_string)
+
+    if time_format.invalid.any?
+      response(400, "Unknown time format #{time_format.invalid}\n")
+    else
+      response(200, "#{time_format.date_time}\n")
+    end
+  end
+end
---
+++ rack/config.ru
@@ -0,0 +1,8 @@
+require_relative 'middleware/runtime'
+require_relative 'middleware/loger'
+require_relative 'time_format'
+require_relative 'app'
+
+use AppLogger, logdev: File.expand_path('log/app.log', __dir__)
+use RunTime
+run App.new
